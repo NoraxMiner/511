788e290d223f73df6f4c78b8381b7f7bd1482b5c
NoraxMiner <NoraxMiner@tutamail.com>
2024-09-29 00:58:27 +0300

    511.1

--- 511.py
+++ 511.py
@@ -6,17 +6,18 @@ K = input_data.read()
 K= int(K)
 Tm = 5
 T1 = 0
-if K >= 250:
-     TK = str("NO")
+if K >= 146:
+     aut = str("NO")
 else:
-     TK = (Tm * K)-Tm
-X=TK // 60
-Y=TK % 60
-X = str(X)
-Y = str(Y)
+     TK = (Tm * K)-Tm    
+     X = TK // 60
+     Y = TK % 60
+     X = str(X)
+     Y = str(Y)
+     aut = str(X + ' ' + Y)
 #--------------------------------------------------------------------
 output_data = open('output.txt', 'w') # Открываем для записи (литера 'w') файл и кладем его в переменную
-output_data.write(str(X + ' ' + Y))
+output_data.write(aut)
 
 # ВАЖНО!!! 
 # не забываем закрывать открытые ранее файлы!
